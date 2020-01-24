6e835de8f447a2b156b49d82114d5e91af12822c
Dick Swart <dick.swart@me.com>
2020-01-24 15:19:04 +1300

    ingredients in progress

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/models/ingredient.model';
 
 @Component({
   selector: 'snr-shopping-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.scss']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient("Apple",2,"units"),
+    new Ingredient("Sugar",6,"tbls"),
+    new Ingredient("Butter",240,"g")
+  ];
 
   constructor() { }
 
