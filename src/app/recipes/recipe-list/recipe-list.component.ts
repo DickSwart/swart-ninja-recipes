cad984dc9eda63141836abfe5d1e1d2361e603cb
Dick Swart <dick.swart@me.com>
2020-01-24 14:29:07 +1300

    added recipe item

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,7 +7,9 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
-  public recipes: Recipe[] = [];
+  public recipes: Recipe[] = [
+    new Recipe('Garlic-Lemon Double Stuffed Chicken Breasts','Not your everyday chicken dish! Stuffed with Cheddar and cream cheeses, then drenched with a garlic-lemon-butter sauce, your friends and family will be begging you to make this recipe - believe me, I know!','http://aucdn.ar-cdn.com/recipes/port250/b85a0f1e-104b-4936-82d3-7b42b1c75897.jpg')
+  ];
 
   constructor() { }
 
